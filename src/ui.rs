57cb98d775cae842f67ce31a5637ba4960362041
Elke Van Santvliet <elkevs96@live.be>
2026-03-02 14:15:48 +0100

    feat: handle user input in new coffee modal and manage draft coffee in state

--- src/ui.rs
+++ src/ui.rs
@@ -19,13 +19,8 @@ const DIALED_IN: &str = r#"
 888  .d88P888888  888888Y8b.    Y88b 888  888  888  888 
 8888888P" 888"Y888888888 "Y8888  "Y888888888888888  888 "#;
 
-const LABELS: [&str; 5] = [
-    "Name: ",
-    "Origin: ",
-    "Varieties: ",
-    "Process: ",
-    "Roaster: ",
-];
+type FieldAccessor = Box<dyn Fn(&DraftCoffee) -> String>;
+type Field = (&'static str, FieldAccessor);
 
 #[derive(Default)]
 struct State {
@@ -37,6 +32,7 @@ struct State {
 struct UiState {
     mode: Mode,
     focus: usize,
+    coffee: Option<DraftCoffee>,
 }
 
 #[derive(Default)]
@@ -44,6 +40,15 @@ enum Mode {
     #[default]
     Normal,
     Add,
+}
+
+#[derive(Default)]
+pub struct DraftCoffee {
+    pub name: Option<String>,
+    pub origin: Option<String>,
+    pub varieties: Option<String>,
+    pub process: Option<String>,
+    pub roaster: Option<String>,
 }
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
@@ -72,20 +77,33 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
             render_coffees(&state.coffees, frame, areas[1]);
 
             if matches!(state.ui_state.mode, Mode::Add) {
-                render_add_coffee_modal(state.ui_state.focus, frame, areas[1]);
+                render_add_coffee_modal(
+                    state.ui_state.focus,
+                    &state.ui_state.coffee,
+                    frame,
+                    areas[1],
+                );
             }
         })?;
 
         if let Event::Key(key) = read()? {
-            match key.code {
-                KeyCode::Char('q') => break Ok(()),
-                KeyCode::Char('a') => state.ui_state.mode = Mode::Add,
-                KeyCode::Tab => {
-                    if matches!(state.ui_state.mode, Mode::Add) {
-                        state.ui_state.focus = (state.ui_state.focus + 1) % LABELS.len();
+            match state.ui_state.mode {
+                Mode::Normal => match key.code {
+                    KeyCode::Char('q') => break Ok(()),
+                    KeyCode::Char('a') => state.ui_state.mode = Mode::Add,
+                    _ => (),
+                },
+                Mode::Add => match key.code {
+                    KeyCode::Tab => {
+                        state.ui_state.focus =
+                            (state.ui_state.focus + 1) % build_draft_fields().len()
                     }
-                }
-                _ => (),
+                    KeyCode::Enter => break Ok(()),
+                    KeyCode::Char(c) => {
+                        update_draft_coffee(&mut state.ui_state.coffee, state.ui_state.focus, c);
+                    }
+                    _ => (),
+                },
             }
         }
     }
@@ -104,7 +122,12 @@ fn render_coffees(coffees: &[Coffee], frame: &mut Frame, area: Rect) {
     frame.render_widget(coffee_list, area);
 }
 
-fn render_add_coffee_modal(focus: usize, frame: &mut Frame, area: Rect) {
+fn render_add_coffee_modal(
+    focus: usize,
+    coffee: &Option<DraftCoffee>,
+    frame: &mut Frame,
+    area: Rect,
+) {
     let modal_area = area.inner(Margin::new(2, 1));
     let modal = Block::bordered().title("New coffee");
     let field_area = modal.inner(modal_area);
@@ -120,20 +143,58 @@ fn render_add_coffee_modal(focus: usize, frame: &mut Frame, area: Rect) {
             Constraint::Length(1),
         ])
         .split(field_area);
-    LABELS
+    build_draft_fields()
         .iter()
         .enumerate()
         .zip(field_areas.iter())
-        .for_each(|((index, label), area)| {
+        .for_each(|((index, (label, value_accessor)), area)| {
             let field_areas = Layout::default()
                 .direction(Direction::Horizontal)
-                .constraints([Constraint::Length(label.len() as u16), Constraint::Min(0)])
+                .constraints([
+                    Constraint::Length(label.len() as u16 + 2),
+                    Constraint::Min(0),
+                ])
                 .split(*area);
             if index == focus {
-                frame.render_widget(Paragraph::new(*label).bg(Color::DarkGray), field_areas[0]);
+                frame.render_widget(
+                    Paragraph::new(format!("{}: ", *label)).bg(Color::DarkGray),
+                    field_areas[0],
+                );
             } else {
-                frame.render_widget(Paragraph::new(*label), field_areas[0]);
+                frame.render_widget(Paragraph::new(format!("{}: ", *label)), field_areas[0]);
             }
-            frame.render_widget(Paragraph::new("input here"), field_areas[1]);
+            let value = coffee.as_ref().map(value_accessor).unwrap_or_default();
+            frame.render_widget(Paragraph::new(value), field_areas[1]);
         });
+}
+
+fn update_draft_coffee(coffee: &mut Option<DraftCoffee>, focus: usize, c: char) {
+    let coffee = coffee.get_or_insert_with(DraftCoffee::default);
+    match focus {
+        0 => coffee.name.get_or_insert_with(String::new).push(c),
+        1 => coffee.origin.get_or_insert_with(String::new).push(c),
+        2 => coffee.varieties.get_or_insert_with(String::new).push(c),
+        3 => coffee.process.get_or_insert_with(String::new).push(c),
+        4 => coffee.roaster.get_or_insert_with(String::new).push(c),
+        _ => (),
+    };
+}
+
+fn build_draft_fields() -> Vec<Field> {
+    vec![
+        ("Name", Box::new(|c| c.name.clone().unwrap_or_default())),
+        ("Origin", Box::new(|c| c.origin.clone().unwrap_or_default())),
+        (
+            "Varieties",
+            Box::new(|c| c.varieties.clone().unwrap_or_default()),
+        ),
+        (
+            "Process",
+            Box::new(|c| c.process.clone().unwrap_or_default()),
+        ),
+        (
+            "Roaster",
+            Box::new(|c| c.roaster.clone().unwrap_or_default()),
+        ),
+    ]
 }
